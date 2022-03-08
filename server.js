3e0b5235c98da32c9ae53cb235f1401a5000255e
Brooke <brookepaglia@gmail.com>
2022-03-08 11:54:41 -0500

    connect to the MySQL database

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'election',
+  },
+  console.log('Connected to the election database.')
+);
 
 // Default response for any not-found request
 app.use((req, res) => {
