f9cdd5d7b5243c3294ebef6c6f04884fe2fe7254
Brooke <brookepaglia@gmail.com>
2022-03-08 11:57:17 -0500

    add db query

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 );
 
-// Default response for any not-found request
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+// Default response for any not-found request (catch all route)
 app.use((req, res) => {
   res.status(404).end();
 });
