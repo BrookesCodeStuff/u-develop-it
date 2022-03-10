2afd68d96a57a2441e0b06912434a3b0492c5577
Brooke <brookepaglia@gmail.com>
2022-03-10 16:27:27 -0500

    build and seed voters table

--- server.js
+++ server.js
@@ -60,6 +60,55 @@ app.get('/api/candidate/:id', (req, res) => {
   });
 });
 
+// CREATE a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?, ?, ?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, results) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body,
+    });
+  });
+});
+
+// UPDATE a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+  const sql = `UPDATE candidates SET party_id = ?
+    WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // Check if a record was found
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows,
+      });
+    }
+  });
+});
+
 // DELETE a candidate
 app.delete('/api/candidate/:id', (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
@@ -79,35 +128,6 @@ app.delete('/api/candidate/:id', (req, res) => {
         id: req.params.id,
       });
     }
-  });
-});
-
-// CREATE a candidate
-app.post('/api/candidate', ({ body }, res) => {
-  const errors = inputCheck(
-    body,
-    'first_name',
-    'last_name',
-    'industry_connected'
-  );
-  if (errors) {
-    res.status(400).json({ error: errors });
-    return;
-  }
-
-  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-  VALUES (?, ?, ?)`;
-  const params = [body.first_name, body.last_name, body.industry_connected];
-
-  db.query(sql, params, (err, results) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body,
-    });
   });
 });
 
@@ -142,28 +162,9 @@ app.get('/api/party/:id', (req, res) => {
   });
 });
 
-// UPDATE a candidate's party
-app.put('/api/candidate/:id', (req, res) => {
-  const sql = `UPDATE candidates SET party_id = ?
-               WHERE id = ?`;
-  const params = [req.body.party_id, req.params.id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      // Check if a record was found
-    } else {
-      res.json({
-        message: 'success',
-        data: req.body,
-        changes: result.affectedRows,
-      });
-    }
-  });
-});
-
 // DELETE a party
 app.delete('/api/party/:id', (req, res) => {
+  // Ensure that a party_id was provided
   const errors = inputCheck(req.body, 'party_id');
   if (errors) {
     res.status(400).json({ error: errors });
