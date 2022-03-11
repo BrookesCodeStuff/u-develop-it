84acca7a93c925aa359f94b39db3e110e1faa820
Brooke <brookepaglia@gmail.com>
2022-03-10 21:42:58 -0500

    create votes table

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -95,7 +95,7 @@ router.delete('/voter/:id', (req, res) => {
 
   db.query(sql, req.params.id, (err, result) => {
     if (err) {
-      res.status(400).json({ error: res.message });
+      res.status(400).json({ error: err.message });
     } else if (!result.affectedRows) {
       res.json({
         message: 'Voter not found',
